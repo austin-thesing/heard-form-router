55bd91c2c88266de23dfbc842549d96718d9213f
Austin Thesing <austin@designxdevelop.com>
2025-03-03 10:53:48 -0700

    Update build

--- src/form-config.js
+++ src/form-config.js
@@ -25,8 +25,7 @@ const FormRouterConfig = {
     income: ["none", "less than $20,000"],
     profession: ["dietician", "nutritionist", "massage therapist", "physical therapist", "dietician or nutritionist", "dietetics or nutrition counseling"],
     practiceRunning: ["opening practice in 1+ month", "opening in 1+ months"],
-    // practiceRunning: ["no"],
-    employeeCount: ["yes (more than 10 employees)"],
+    employeeCount: ["yes (more than 10 employees)".toLowerCase()],
   },
 
   INCOME_TIERS: {
